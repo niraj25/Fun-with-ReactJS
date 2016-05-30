3adf4c579d0cb307926bcf6271ae18a0b17bcf6a
Niraj <dev.momentuum@yahoo.ca>
2016-05-29 22:36:30 -0400

    testing render function commit

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 class App extends React.Component{
 	render(){
-		return <div>Hello World from React</div>;
+		return (
+			<div>
+				<div>Hello World from React</div>
+				<b>This is real fun!!!</b>
+			</div>
+		);
 	}
 }
 export default App
